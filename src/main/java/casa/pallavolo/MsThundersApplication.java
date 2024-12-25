068dc50a6cb1d33055b1a48dc4bb69f6aeb05842
emanuele <eperoni76@gmail.com>
2024-12-25 12:38:49 +0100

    initial commit

---
+++ src/main/java/casa/pallavolo/MsThundersApplication.java
@@ -0,0 +1,13 @@
+package casa.pallavolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsThundersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsThundersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/casa/pallavolo/MsThundersApplicationTests.java
@@ -0,0 +1,13 @@
+package casa.pallavolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsThundersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
